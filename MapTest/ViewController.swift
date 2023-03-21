e087366b1ae6215fdc36696ec44df1495c82ae79
Mikita <nikita_popow_1993@mail.ru>
2023-03-21 13:04:04 +0300

    Initial Commit

---
+++ MapTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by Никита Попов on 21.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
